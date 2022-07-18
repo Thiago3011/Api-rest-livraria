74f36fa6be5071aca1e55c75b266132cd53a4caf
Thiago3011 <thiago.silva1001@etec.sp.gov.br>
2022-07-18 20:37:03 -0300

    rotas, crud e etc

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1, 
+        'titulo': 'Senhor dos aneis'
+    },
+
+    {
+        id: 2, 
+        'titulo': 'Senhor dos aneis'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+
+    res.status(201).json(livros[index])
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+
+    livros[index].titulo = req.body.titulo
+
+    res.status(201).json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params
+    let index = buscaLivro(id)
+
+    livros.splice(index, 1)
+
+    res.status(201).send(`Livro ${id} removido com sucesso`)
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
+export default app
